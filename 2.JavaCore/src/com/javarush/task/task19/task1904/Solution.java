c76cffc057a58c7227460de50cd94c27cac8f268
Alexey <oberonru@mail.ru>
2019-06-06 21:18:23 +0600

    06.06.2019

--- 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -1,33 +1,37 @@
 package com.javarush.task.task19.task1904;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
  * И еще один адаптер
  * Адаптировать Scanner к PersonScanner.
  * Классом-адаптером является PersonScannerAdapter.
- * В классе адаптере создать приватное финальное поле Scanner fileScanner. Поле инициализировать в конструкторе с одним
- * аргументом типа Scanner.
- *
+ * В классе адаптере создать приватное финальное поле Scanner fileScanner. Поле инициализировать в
+ * конструкторе с одним аргументом типа Scanner.
+ * <p>
  * Данные в файле хранятся в следующем виде:
  * Иванов Иван Иванович 31 12 1950
  * Петров Петр Петрович 31 12 1957
- *
- * В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен
- * читать данные только одного человека.
- *
- *
+ * <p>
+ * В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read()
+ * должен читать данные только одного человека.
+ * <p>
+ * <p>
  * Требования:
  * 1. PersonScanner должен быть интерфейсом.
  * 2. Класс PersonScannerAdapter должен реализовывать интерфейс PersonScanner.
  * 3. Класс PersonScannerAdapter должен содержать приватное поле fileScanner типа Scanner.
  * 4. Класс PersonScannerAdapter должен содержать конструктор с параметром Scanner.
  * 5. Метод close() класса PersonScannerAdapter должен делегировать полномочие такому же методу fileScanner.
- * 6. Метод read() класса PersonScannerAdapter должен читать строку с файла, парсить её, и возвращать данные только
- * одного человека, в виде объекта класса Person.
-И еще один адаптер
-*/
+ * 6. Метод read() класса PersonScannerAdapter должен читать строку с файла, парсить её, и возвращать данные
+ * только одного человека, в виде объекта класса Person.
+ * И еще один адаптер
+ */
 
 public class Solution {
 
@@ -36,23 +40,23 @@ public class Solution {
     }
 
     public static class PersonScannerAdapter implements PersonScanner {
-
-        private final Scanner fileScanner;
+        private Scanner fileScanner;
 
         public PersonScannerAdapter(Scanner fileScanner) {
             this.fileScanner = fileScanner;
         }
 
-        @Override
-        public Person read() throws IOException {
-            return null;
+        public void close() {
+            this.fileScanner.close();
         }
 
-        @Override
-        public void close() throws IOException {
-            fileScanner.close();
+        public Person read() throws IOException, ParseException {
+            String[] array = fileScanner.nextLine().split(" ");
+            String firstName = array[0];
+            String middleName = array[1];
+            String lastName = array[2];
+            Date date = new SimpleDateFormat("dd/MM/yyyy").parse( String.format("%s/%s/%s", array[3], array[4], array[5]) );
+            return new Person(lastName, firstName, middleName, date);
         }
     }
-
-
 }
--- 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -1,11 +1,6 @@
 package com.javarush.task.task19.task1907;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 
 /**
  * Считаем слово
@@ -13,27 +8,31 @@ import java.util.List;
  * Файл содержит слова, разделенные знаками препинания.
  * Вывести в консоль количество слов "world", которые встречаются в файле.
  * Закрыть потоки.
- *
- *
+ * <p>
+ * <p>
  * Требования:
  * 1. Программа должна считывать имя файла с консоли (используй BufferedReader).
  * 2. BufferedReader для считывания данных с консоли должен быть закрыт.
  * 3. Программа должна считывать содержимое файла (используй FileReader c конструктором принимающим String).
  * 4. Поток чтения из файла (FileReader) должен быть закрыт.
  * 5. Программа должна выводить в консоль количество слов "world", которые встречаются в файле.
-Считаем слово
-*/
+ * Считаем слово
+ */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = reader.readLine();
-        reader.close();
-        List<String> list = new ArrayList<>();
-        FileReader fileReader = new FileReader(fileName);
-        while (fileReader.ready()) {
-            list.add(String.valueOf(fileReader.read()));
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()))) {
+            int count = 0;
+            while (fileReader.ready()) {
+                String[] line = fileReader.readLine().split("\\W");
+                for (int i = 0; i < line.length; i++) {
+                    if (line[i].equals("world")) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
         }
-
     }
 }
